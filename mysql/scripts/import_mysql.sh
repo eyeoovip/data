b7791c012dc94d44f14f1075bdcdbd8cfe145259
王龙军 <516364218@qq.com>
2024-03-23 00:38:57 +0800

    init

--- mysql/scripts/import_mysql.sh
+++ mysql/scripts/import_mysql.sh
@@ -4,4 +4,4 @@ DOCKER_CONN="docker exec -i mysql"
 
 #导入数据
 echo -e "\e[34mimport database:\e[0m"
-${DOCKER_CONN} mysql -ucreshop -p'N4F3ABaFui' -f < /data/mysql/backups/b_edu_com.sql
+${DOCKER_CONN} mysql -uroot -p'RZ3X5mh8LUqkWNSh' -f < /data/mysql/backups/b_edu_com.sql
